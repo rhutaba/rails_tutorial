58cfc00baa6e6af59a096642b87f90c90e90eadf
JuntetsuTei <teijuntetsu@gmail.com>
2017-06-05 02:17:59 +0000

    temp

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
+  test "unsuccsessful edit" do
+    log_in_as(@user)
+    # editページにアクセスできるか確認
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    
+    # 無効なユーザ編集を送信した場合、editページにリダイレクトすることを確認
+    patch user_path(@user), params: {
+      user: {
+        name: "",
+        email: "foo@invalid",
+        password: "foo",
+        password_confirmation: "bar"
+      }
+    }
+    assert_template 'users/edit'
+  end
+  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    
+    patch user_path(@user), params: { 
+      user: {
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+end
